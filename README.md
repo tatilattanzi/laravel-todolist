dc8acd2f70977e7ec6284c0cb90b4dae5b11f3c8
TaTi Lattanzi <tati_lattanzi@hotmail.com>
2017-10-07 18:00:54 -0300

    Quickstart

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# laravel-todolist
+# Laravel - ToDo List
+
+## Instructions
+<ul>
+<li>After cloning this repository, go to the root folder, run the following command's:
+<pre>
+    composer install
+</pre>
+</li>
+<li>Rename .env.example to .env and provide your database details there.</li>
+<li>It needs a database table to perform CRUD operations on it. To import the table, run:
+<pre>php artisan migrate</pre>
+</li>
+<li>Generating a New Application Key
+<pre>php artisan key:generate</pre>
+
+</li>
+<li>Run 
+<pre>php artisan serve</pre> </li>
+</ul>
---
+++ app/Http/routes.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:5|max:50',
+            'description' => 'max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Update Task
+     */
+     Route::patch('/task/{id}', function ($id) {
+        $task = Task::findOrFail($id);
+
+        $task->done = !$task->done;
+        $task->save();
+
+        return redirect('/');
+     });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+});
